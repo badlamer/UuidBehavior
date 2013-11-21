16c96cf0dcbb2b990d8254b278412ebb4961042b
Yuri Druzhkov <bad1lamer@gmail.com>
2013-11-21 17:37:18 +0400

    remove short tag

--- src/templates/addPermanentUuidUpdateHook.php
+++ src/templates/addPermanentUuidUpdateHook.php
@@ -2,7 +2,7 @@
 * If permanent UUID, throw exception <?php echo $uuidColumn ?>
 */
 public function preUpdate(PropelPDO $con = NULL) {
-	<? if($permanent) :?>
+	<?php if($permanent) :?>
 	if(!$this->isNew()) {
 		if($this->isColumnModified('<?php echo $uuidColumn ?>')) {
 			throw new \InvalidArgumentException('You can not change the permanent UUID');
